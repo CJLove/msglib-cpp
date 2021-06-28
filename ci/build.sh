5755e7f8ee60545b83bd40205d70337c08aef1c0
Chris Love <christopherlove68@gmail.com>
2021-06-27 19:10:41 -0700

    CI step #1

---
+++ ci/build.sh
@@ -0,0 +1,16 @@
+#/bin/bash
+
+[ ! -d ./msglib-cpp-git ] && { echo "ERROR: repo not cloned!"; exit 1; }
+
+# Change to the base directory of the repo
+cd msglib-cpp-git
+
+# Create build directory and switch to it
+mkdir -p build
+cd build
+
+# Configure via CMake
+cmake -DCMAKE_BUILD_TYPE=DEBUG -DBUILD_EXAMPLES=OFF -DBUILD_TEST=ON ..
+
+# Build
+make
